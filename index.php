41d9225e1670a2dbe105da81880d0d45a90996e1
christopherabate <contact@christopherabate.com>
2016-05-21 01:33:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, Chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechLunch</title>
+    <style>
+	    h1 {
+	    	color: #FF0066;
+	    }
+    </style>
+</head>
+<body>
+    <h1>TechLunch</h1>
+    <p>Un pas vers les processus de développements et déploiements centralisés</p>
+</body>
+</html>
